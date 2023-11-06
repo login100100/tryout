5698c92b48df56b63f6a87d1ca152d8504ff2a76
mradomski <login100100100@gmail.com>
2023-11-06 23:18:41 +0100

    passing crypto data via extended localstorage provider

--- src/app.storages.ts
+++ src/app.storages.ts
@@ -3,6 +3,7 @@
 /*****************************************/
 /* LocalStorage list  */
 export const LOCALSTORAGE_THEME = "theme";
+export const LOCALSTORAGE_CRYPTO = "crypto";
 export const LOCALSTORAGE_LANGUAGE = "language";
 export const LOCALSTORAGE_CRYPTO_CURRENCY = "crypto_currency";
 export const LOCALSTORAGE_CRYPTO_FAVORITES = "crypto_favorites";
